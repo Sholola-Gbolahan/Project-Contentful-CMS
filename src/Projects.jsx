92321e5c2f6cdba12f2177613695f5e5a9e8b046
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-08-11 23:28:52 +0100

    How to ~ fetch data using aysnc approach.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,10 @@
 import React from "react"
-import "./fetchProjects"
+import { useFetchProjects } from "./fetchProjects"
 
 const Projects = () => {
+  const { isLoading, projects } = useFetchProjects()
+  console.log(isLoading, projects)
+
   return <h2>Projects</h2>
 }
 
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,4 +1,5 @@
 import { createClient } from "contentful"
+import { useEffect, useState } from "react"
 
 const client = createClient({
   space: "21mr5uy6q5fg",
@@ -6,9 +7,29 @@ const client = createClient({
   accessToken: "A_IhglAGIOuJg4gT8wUCBM0c018ZsQkgWwFbl7bVqQo",
 })
 
-client
-  .getEntries({
-    content_type: "projects",
-  })
-  .then((response) => console.log(response))
-  .catch(console.error)
+export const useFetchProjects = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: "projects" })
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+        return { id, title, img, url }
+      })
+      setProjects(projects)
+
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return { isLoading, projects }
+}
